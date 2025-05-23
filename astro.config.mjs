b869b64bc136a92419d445758ca2908d140d7425
Oziel Pacheco <oziel.pacheco@ducter.com.mx>
2025-05-23 04:48:49 -0600

    Agregamos .env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "http://localhost";
+
 
 // https://astro.build/config
 export default defineConfig({
+  site: `${host}:${port}`, //Solo funciona en build
+  trailingSlash: "always", // agrega barra al final de las URLs
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
